6a70f5f7121d656414bfc4e40edb8f9e3d43279e
Geidy <geidydm@gmail.com>
2019-06-10 06:34:16 -0400

    Updates

---
+++ precache-manifest.49f90e7a594664c636a853cf9c1a6a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359dfb0501498852baadd23e4d78660c",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "a9ed75ae1c9467b6ca07",
+    "url": "/repository-name/static/css/main.aa95821e.chunk.css"
+  },
+  {
+    "revision": "b4698518f559bf4ba2d6",
+    "url": "/repository-name/static/js/2.ff47f192.chunk.js"
+  },
+  {
+    "revision": "a9ed75ae1c9467b6ca07",
+    "url": "/repository-name/static/js/main.55fe2d26.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "7aff1100667c420fa445c4001cdb647e",
+    "url": "/repository-name/static/media/plateau_nida.7aff1100.jpg"
+  }
+]);
